d99d59b191a398bb6e73b3ccb2fb1882137dba8d
wangkx <1989163733@qq.com>
2023-03-28 16:57:14 +0800

    实现大图浏览

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Openeadragon from './components/Openseadragon.vue';
+</script>
+
+<template>
+  <Openeadragon></Openeadragon>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Openseadragon.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import  OpenSeadragon  from 'openseadragon';
+
+onMounted(()=>{
+  initOpenSeadragon()
+})
+
+const getImgUrl = (name: string) => {
+  return new URL('../assets/' + name, import.meta.url).href
+}
+
+const initOpenSeadragon = ()=>{
+  OpenSeadragon({
+    id: "openseadragon1", //body中挂载的id
+    prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+    tileSources: getImgUrl('snt.dzi'),
+    showNavigator:true,
+      // Initial rotation angle
+      degrees: 0,
+      // Show rotation buttons
+      showRotationControl: true,
+      // Enable touch rotation on tactile devices
+      gestureSettingsTouch: <any>{
+          pinchRotate: true
+      }
+  })
+}
+
+</script>
+
+<template>
+  <div id="openseadragon1"></div>
+</template>
+
+<style scoped>
+#openseadragon1{
+  width: 100%;
+  height: 800px;
+}
+</style>
